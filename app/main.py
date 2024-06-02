51c3c1844f57f416d59dd68637dd7fafdd9ea47f
Caceresenzo <caceresenzo1502@gmail.com>
2024-06-02 16:20:47 +0200

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -5,7 +5,11 @@ def main():
     sys.stdout.write("$ ")
     sys.stdout.flush()
 
-    input()
+    line = input()
+    arguments = line.split(" ")
+
+    program = arguments[0]
+    print(f"{program}: command not found")
 
 
 if __name__ == "__main__":
