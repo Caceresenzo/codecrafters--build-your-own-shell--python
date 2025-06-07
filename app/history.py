85ccb1ca2c4180c7b162d17fef838e1cbb25f1f0
Caceresenzo <caceresenzo1502@gmail.com>
2025-06-07 16:04:31 +0200

    CLI tests (cli-test-1749305071225)

--- app/history.py
+++ app/history.py
@@ -1,3 +1,7 @@
+import os
+
+FILE_ENVVAR = "HISTFILE"
+
 previous_lines = []
 last_append_index = 0
 
@@ -32,3 +36,14 @@ def write(path: str, append=False):
             fd.write(line + "\n")
     
     last_append_index = len(previous_lines)
+
+
+def initialize():
+    path = os.environ.get(FILE_ENVVAR)
+    if not path:
+        return
+
+    if not os.path.isfile(path):
+        return
+
+    read(path)
