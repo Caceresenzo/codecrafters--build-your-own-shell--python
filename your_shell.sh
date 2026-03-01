846d1eb65a5b6603b584a5a42b7e78b30eb54c57
Caceresenzo <caceresenzo1502@gmail.com>
2026-03-01 16:53:00 +0100

    CLI tests (cli-test-1772380380619)

--- your_shell.sh
+++ your_shell.sh
@@ -5,4 +5,9 @@
 # CodeCrafters uses this file to test your code. Don't make any changes here!
 #
 # DON'T EDIT THIS!
+pipenv run python3 -c '' >/dev/null 2>&1
+SCRIPT_DIR="$(dirname "$0")"
+cd "$SCRIPT_DIR"
+pwd
+find
 exec pipenv run python3 -m app.main "$@"
